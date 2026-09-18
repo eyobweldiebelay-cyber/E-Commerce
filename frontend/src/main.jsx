ca7c60773f2a14a0ca7656142a8ca7bf26551187
eyob <eyobweldiebelay@gmail.com>
2026-09-18 05:33:49 +0300

    main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.jsx'
+
+import { AuthProvider } from './context/AuthContext'
+import { CartProvider } from './context/CartContext'
+
+import './styles/global.css'
+import './styles/navbar.css'
+import './styles/forms.css'
+import './styles/home.css'
+import './styles/product.css'
+import './styles/cart.css'
+import './styles/dashboard.css'
+import './styles/responsive.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </AuthProvider>
   </StrictMode>,
 )
